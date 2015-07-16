9620ca2964d14ea0ea8a43a56d39d9dc14982ff3
Matt Smith <runner_28@hotmail.com>
2015-07-16 10:06:38 -0400

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,15 @@
+/*global jQuery */
+if (jQuery) {
+  (function ($) {
+    'use strict';
+    $(document).ready(function () {
+      // initialize the megamenu
+      $('.megamenu').accessibleMegaMenu();
+
+      // hack so that the megamenu doesn't show flash of css animation after the page loads.
+      setTimeout(function () {
+        $('body').removeClass('init');
+      }, 500);
+    });
+  }(jQuery));
+}
